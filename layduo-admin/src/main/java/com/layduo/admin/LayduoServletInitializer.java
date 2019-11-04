380fdbf4930fa99bb383ad7239382b4ad371234f
layduo <1260958005@qq.com>
2019-11-04 19:36:10 +0800

    第一次提交

---
+++ layduo-admin/src/main/java/com/layduo/admin/LayduoServletInitializer.java
@@ -0,0 +1,19 @@
+package com.layduo.admin;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+/**
+ * web容器中部署
+ * @author layduo
+ *
+ */
+public class LayduoServletInitializer extends SpringBootServletInitializer{
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(LayduoApplication.class);
+	}
+
+	
+}
